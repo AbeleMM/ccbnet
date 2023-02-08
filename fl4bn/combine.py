1b05d54a9628bd1a537f425f18f7833e8c9e8a2f
Abele Malan <abele.malan@asml.com>
2023-02-08 11:44:32 +0100

    Add first version of the project

---
+++ fl4bn/combine.py
@@ -0,0 +1,144 @@
+import itertools
+from collections import defaultdict
+from enum import Enum
+from typing import cast
+
+import numpy as np
+from pgmpy.factors.discrete.CPD import TabularCPD
+from pgmpy.models import BayesianNetwork
+
+
+class CombineMethod(Enum):
+    MULTI = "Multi"
+    SINGLE = "Single"
+    UNION = "Union"
+
+
+def combine_bns(bns: list[BayesianNetwork], method: CombineMethod) -> BayesianNetwork:
+    return combine_bns_weighted({bn: 1 for bn in bns}, method)
+
+
+def combine_bns_weighted(
+        bn_to_conf: dict[BayesianNetwork, float],
+        method: CombineMethod) -> BayesianNetwork:
+
+    if method is CombineMethod.MULTI:
+        return _combine_bns_weighted_multi(bn_to_conf)
+
+    node_to_bns: dict[str, list[BayesianNetwork]] = defaultdict(list)
+    node_to_vals: dict[str, list[int | str]] = {}
+    bn_combined = BayesianNetwork()
+
+    for bayes_net in bn_to_conf:
+        node_to_vals.update(bayes_net.states)
+        for node in cast(list[str], bayes_net.nodes()):
+            node_to_bns[node].append(bayes_net)
+
+    for node, node_bns in node_to_bns.items():
+        bn_combined.add_node(node)
+        # Begin Algorithm 1
+        if len(node_bns) == 1:
+            _preserve_from_bn(bn_combined, node, node_bns[0])
+        else:
+            intersect_node_bns: set[str] = set.intersection(*[
+                cast(set[str], set(bn.nodes())) for bn in node_bns
+            ])
+            node_bn_to_parent_set: dict[BayesianNetwork, set[str]] = {
+                bn: set(bn.get_parents(node)) for bn in node_bns
+            }
+            bns_without_intersect_node_parents: list[BayesianNetwork] = [
+                bn
+                for bn, parent_set in node_bn_to_parent_set.items()
+                if parent_set - intersect_node_bns
+            ]
+            # Interior Node
+            if (
+                len(bns_without_intersect_node_parents) < len(node_bn_to_parent_set) and
+                method != CombineMethod.UNION
+            ):
+                # Begin Algorithm 2
+                if len(bns_without_intersect_node_parents) == 0:
+                    bn_perserve = max(
+                        node_bn_to_parent_set.keys(),
+                        key=lambda bn: bn_to_conf[bn]
+                    )
+                    _preserve_from_bn(bn_combined, node, bn_perserve)
+                elif len(bns_without_intersect_node_parents) == 1:
+                    _preserve_from_bn(bn_combined, node, bns_without_intersect_node_parents[0])
+                else:
+                    bn_perserve = max(
+                        bns_without_intersect_node_parents,
+                        key=lambda bn: bn_to_conf[bn]
+                    )
+                    _preserve_from_bn(bn_combined, node, bn_perserve)
+                # End Algorithm 2
+            # Exterior Node
+            else:
+                # Begin Algorithm 3
+                parents_union: list[str] = sorted(set.union(*node_bn_to_parent_set.values()))
+                bn_combined.add_edges_from([(p, node) for p in parents_union])
+
+                node_cpd = TabularCPD(
+                    variable=node,
+                    variable_card=len(node_to_vals[node]),
+                    values=_get_ext_node_values(node, parents_union, node_to_vals, node_bns),
+                    evidence=parents_union,
+                    evidence_card=[len(node_to_vals[p]) for p in parents_union],
+                    state_names={node_: node_to_vals[node_] for node_ in [node, *parents_union]}
+                )
+
+                node_cpd.normalize(inplace=True)
+                bn_combined.add_cpds(node_cpd)
+                # End Algorithm 3
+        # End Algorithm 1
+
+    return bn_combined
+
+
+def _combine_bns_weighted_multi(bn_to_conf: dict[BayesianNetwork, float]) -> BayesianNetwork:
+    iter_bn_to_conf = iter(bn_to_conf.items())
+    bn_combined, conf_combined = next(iter_bn_to_conf)
+    for bayes_net, confidence in iter_bn_to_conf:
+        bn_combined = combine_bns_weighted(
+            {bn_combined: conf_combined, bayes_net: confidence},
+            CombineMethod.SINGLE
+        )
+        conf_combined = (conf_combined + confidence) / 2
+    return bn_combined
+
+
+def _preserve_from_bn(tgt_bn: BayesianNetwork, node: str, src_bn: BayesianNetwork) -> None:
+    tgt_bn.add_edges_from([(p, node) for p in src_bn.get_parents(node)])
+    tgt_bn.add_cpds(src_bn.get_cpds(node))
+
+
+def _get_superposition(l_val: float, r_val: float) -> float:
+    return l_val + r_val - l_val * r_val
+
+
+def _get_ext_node_values(
+        node: str, parents_union: list[str],
+        node_to_vals: dict[str, list], node_bns: list[BayesianNetwork]) -> np.ndarray:
+    nr_node_vals: int = len(node_to_vals[node])
+    transp_table: list[list[float]] = []
+
+    for parent_inst in itertools.product(*[node_to_vals[parent] for parent in parents_union]):
+        tt_row: list[float] = [0] * nr_node_vals
+        for bayes_net in node_bns:
+            cpd: TabularCPD = cast(TabularCPD, bayes_net.get_cpds(node))
+            values: np.ndarray = cast(
+                TabularCPD,
+                cpd.reduce(
+                    [
+                        (parent, parent_inst[i])
+                        for i, parent in enumerate(parents_union)
+                        if parent in cpd.get_evidence()
+                    ],
+                    inplace=False
+                )
+            ).values
+            # TODO incorporate weights
+            tt_row = [_get_superposition(v, values[i]) for i, v in enumerate(tt_row)]
+        transp_table.append(tt_row)
+
+    return np.array(transp_table).T
---
+++ fl4bn/util.py
@@ -0,0 +1,178 @@
+import warnings
+from collections import defaultdict
+from random import Random
+from typing import Collection, cast
+
+from combine import CombineMethod, combine_bns
+from pandas import DataFrame
+from pgmpy.base import DAG
+from pgmpy.estimators import HillClimbSearch, MaximumLikelihoodEstimator
+from pgmpy.inference import VariableElimination
+from pgmpy.models import BayesianNetwork
+from pgmpy.sampling import BayesianModelSampling
+from pgmpy.utils import get_example_model
+
+
+def print_bn(bayes_net: BayesianNetwork, struct_only=False) -> None:
+    if not struct_only:
+        for cpd in bayes_net.get_cpds() or []:
+            print(cpd)
+            print()
+    bayes_net.to_daft().render()
+
+
+def get_in_out_nodes(bayes_net: BayesianNetwork) -> tuple[list[str], list[str]]:
+    # Island nodes are not included in neither the in nor the out list.
+    in_nodes: list[str] = []
+    out_nodes: list[str] = []
+    edges_to, edges_from = [set(edge_list) for edge_list in zip(*bayes_net.edges())]
+
+    for node in cast(Collection[str], bayes_net.nodes()):
+        if node not in edges_to and node in edges_from:
+            out_nodes.append(node)
+        elif node not in edges_from and node in edges_to:
+            in_nodes.append(node)
+
+    return (in_nodes, out_nodes)
+
+
+def split_vars(bayes_net: BayesianNetwork, nr_splits: int, r_seed: int | None) -> list[list[str]]:
+    # Assumes nr. connected components in network <= nr. splits
+    # Based on Kruskal's Algorithm
+    node_parent: dict[str, str] = {}
+    node_rank: dict[str, int] = {}
+    shuffled_edges = cast(list[tuple[str, str]], list(bayes_net.edges()))
+    Random(r_seed).shuffle(shuffled_edges)  # nosec
+    nr_edges = 0
+
+    for node in cast(Collection[str], bayes_net.nodes()):
+        node_parent[node] = node
+        node_rank[node] = 0
+
+    while nr_edges < len(bayes_net.nodes()) - nr_splits:
+        node_out, node_inc = shuffled_edges.pop()
+        parent_node_out = _find_parent(node_out, node_parent)
+        parent_node_inc = _find_parent(node_inc, node_parent)
+
+        if parent_node_out != parent_node_inc:
+            nr_edges += 1
+            if node_rank[parent_node_out] < node_rank[parent_node_inc]:
+                node_parent[parent_node_out] = node_parent[parent_node_inc]
+            else:
+                node_parent[parent_node_inc] = node_parent[parent_node_out]
+                inc = node_rank[parent_node_out] == node_rank[parent_node_inc]
+                node_rank[parent_node_out] += inc
+
+    parent_to_nodes: dict[str, list[str]] = defaultdict(list)
+    crossed_parents: set[str] = set()
+
+    for node in node_parent:
+        parent_to_nodes[_find_parent(node, node_parent)].append(node)
+
+    for node_out, node_inc in shuffled_edges:
+        parent_node_out = node_parent[node_out]
+        parent_node_inc = node_parent[node_inc]
+        if (
+            parent_node_out != parent_node_inc and
+            not set([parent_node_out, parent_node_inc]) <= crossed_parents
+        ):
+            crossed_parents.add(parent_node_out)
+            crossed_parents.add(parent_node_inc)
+            parent_to_nodes[parent_node_out].append(node_inc)
+            parent_to_nodes[parent_node_inc].append(node_out)
+
+    return list(parent_to_nodes.values())
+
+
+def train_model(samples: DataFrame) -> BayesianNetwork:
+    est = HillClimbSearch(data=samples, use_cache=True)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", FutureWarning)
+        estimated_dag = cast(
+            DAG,
+            est.estimate(
+                scoring_method="k2score",
+                max_indegree=len(samples.columns) // 2,
+                max_iter=10**4,
+                show_progress=False
+            )
+        )
+
+    bayes_net = BayesianNetwork()
+    bayes_net.add_nodes_from(samples.columns)
+    bayes_net.add_edges_from(estimated_dag.edges())
+
+    bayes_net.fit(
+        samples,
+        estimator=MaximumLikelihoodEstimator,
+        complete_samples_only=True
+    )
+
+    return bayes_net
+
+
+def calc_accuracy(
+        test_df: DataFrame, bayes_net: BayesianNetwork,
+        e_vars: list[str], q_vars: list[str]) -> dict[str, float]:
+    bn_infer = VariableElimination(bayes_net)
+    accuracy = {v: 0 for v in q_vars}
+
+    for _, row in test_df.iterrows():
+        query = cast(
+            dict[str, str],
+            bn_infer.map_query(
+                variables=q_vars,
+                evidence={v: row[v] for v in e_vars},
+                show_progress=False
+            )
+        )
+        for variable, value in query.items():
+            accuracy[variable] += row[variable] == value
+
+    accuracy = {k: round(v / len(test_df) * 100, 3) for (k, v) in accuracy.items()}
+    return accuracy
+
+
+def benchmark(
+        name_bn: str,
+        nr_clients: int,
+        test_counts: int,
+        samples_per_node: int = 5000,
+        r_seed: int | None = None) -> dict[str, dict[str, float]]:
+    model = get_example_model(name_bn)
+    sampling = BayesianModelSampling(model)
+    clients_train_vars = split_vars(model, nr_clients, r_seed)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", UserWarning)
+        clients_train_samples = [
+            sampling.forward_sample(
+                size=samples_per_node * len(vars),
+                seed=r_seed,
+                show_progress=False
+            )[vars]
+            for vars in clients_train_vars
+        ]
+        test_samples = sampling.forward_sample(size=test_counts, seed=r_seed, show_progress=False)
+    evidence_vars, query_vars = get_in_out_nodes(model)
+    trained_models = [train_model(client_samples) for client_samples in clients_train_samples]
+    res = {
+        "Ground truth": calc_accuracy(test_samples, model, evidence_vars, query_vars)
+    }
+    for method in CombineMethod:
+        res[method.value] = calc_accuracy(
+            test_samples,
+            # TODO switch to combine_bns_weighted (by amount of samples)
+            combine_bns(trained_models, method),
+            evidence_vars,
+            query_vars
+        )
+    return res
+
+
+def _find_parent(node: str, node_to_parent: dict[str, str]) -> str:
+    parent = node
+    while node_to_parent[parent] != parent:
+        node_to_parent[parent] = node_to_parent[node_to_parent[parent]]
+        parent = node_to_parent[parent]
+    return parent
