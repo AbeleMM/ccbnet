54e1bb41208646fa001e8e15d82190a4a486f87d
Abele Malan <abele.malan@asml.com>
2023-05-27 14:23:48 +0200

    Streamline inference procedure

--- fl4bn/model.py
+++ fl4bn/model.py
@@ -42,28 +42,21 @@ class Model(ABC):
 
 def var_elim(targets: list[str], evidence: dict[str, str], factors: list[DiscreteFactor],
              nodes: Collection[str]) -> DiscreteFactor:
-    node_to_facts: dict[str, list[DiscreteFactor]] = defaultdict(list)
-
     facts = factors.copy()
 
-    for fact in facts:
-        for node in cast(list[str], fact.variables):
-            node_to_facts[node].append(fact)
-
     for node in sorted(n for n in nodes if n not in evidence and n not in targets):
-        rel_facts = node_to_facts[node].copy()
+        rel_facts: list[DiscreteFactor] = []
+        new_facts: list[DiscreteFactor] = []
 
-        facts = [fact for fact in facts if fact not in rel_facts]
+        for fact in facts:
+            if node in fact.variables:
+                rel_facts.append(fact)
+            else:
+                new_facts.append(fact)
 
         prod_rel_facts = cast(DiscreteFactor, factor_product(*rel_facts))
         prod_rel_facts.marginalize([node], inplace=True)
-
-        for node_, node_facts in node_to_facts.items():
-            node_to_facts[node_] = [fact for fact in node_facts if fact not in rel_facts]
-
-        for prf_var in cast(list[str], prod_rel_facts.variables):
-            node_to_facts[prf_var].append(prod_rel_facts)
-
+        facts = new_facts
         facts.append(prod_rel_facts)
 
     prod_facts = cast(DiscreteFactor, factor_product(*facts))
